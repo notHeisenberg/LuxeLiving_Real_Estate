d25f250315de5fa936a25f284b7a87b8f890e92b
notHeisenberg007 <mahfuzantor2@gmail.com>
2024-04-15 21:48:36 +0600

    env firebase file done

--- firebase.config.js
+++ firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA3694zNenOk3onfcLOnPyyVY_6yb6Scj4",
-    authDomain: "real-state-30c8b.firebaseapp.com",
-    projectId: "real-state-30c8b",
-    storageBucket: "real-state-30c8b.appspot.com",
-    messagingSenderId: "339727227760",
-    appId: "1:339727227760:web:9f6ba1c51c44bcad724db5"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
